e642b37782167c119755277be4c7f832c952c12e
Marcel Schramm <marcel.schramm@vier.ai>
2023-11-15 13:19:44 +0100

    Delete unused local var

--- init.lua
+++ init.lua
@@ -349,7 +349,6 @@ require('mason-lspconfig').setup()
 --
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
-local util = require 'lspconfig.util'
 local servers = {
   gopls = {},
   tsserver = {},
