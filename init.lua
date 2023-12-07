da7e37ec09f97c70c458780e6086b1ced1414797
Marcel Schramm <marcel.schramm@vier.ai>
2023-12-07 14:01:26 +0100

    Specify luasnip as latest v1, since v2 breaks stuff

--- init.lua
+++ init.lua
@@ -120,7 +120,10 @@ require('lazy').setup({
     'hrsh7th/nvim-cmp',
     dependencies = {
       -- Snippet Engine & its associated nvim-cmp source
-      'L3MON4D3/LuaSnip',
+      {
+        'L3MON4D3/LuaSnip',
+        version = 'v1.*',
+      },
       'saadparwaiz1/cmp_luasnip',
 
       -- Adds LSP completion capabilities
@@ -400,7 +403,6 @@ mason_lspconfig.setup_handlers {
 -- See `:help cmp`
 local cmp = require 'cmp'
 local luasnip = require 'luasnip'
--- require('luasnip.loaders.from_vscode').lazy_load()
 luasnip.config.setup {}
 
 ---@diagnostic disable-next-line: missing-fields
