8e2a84c4bccd6b6ed7260e62e3215e217c6b48d2
Marcel Schramm <marcel.schramm@lynq.tech>
2024-02-12 12:54:47 +0100

    Find Files now also shows hidden files

--- init.lua
+++ init.lua
@@ -216,8 +216,12 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
 
+local find_files = function()
+  require('telescope.builtin').find_files({ find_command = { 'rg', '--files', '--no-ignore-vcs', '--hidden', '-g', '!.git' } })
+end
+
 vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sf', find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
