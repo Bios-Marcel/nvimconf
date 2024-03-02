e259c5ca4318810c6b16a3a8f7afd9e31983752f
Marcel Schramm <marcel.schramm@lynq.tech>
2024-03-02 13:36:34 +0100

    Disable float transparency, it is confusing AF

--- init.lua
+++ init.lua
@@ -608,7 +608,6 @@ vim.cmd("colorscheme catppuccin-macchiato")
 
 -- Basic transparency; Doesn't do floats rn.
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
 -- Alternatively use 'xiyaowong/transparent.nvim'
